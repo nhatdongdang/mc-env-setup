083db99cc8b8dde2d9095f531c6f166421ec4863
Dan <144138246+nhatdongdang@users.noreply.github.com>
2024-06-19 00:02:34 +1000

    Update .sh files

---
+++ join_spigot.sh
@@ -0,0 +1,14 @@
+cd Minecraft_Tools
+cd MinecraftClient
+
+touch output.log
+./MinecraftClient > output.log &
+
+echo "Joining Spigot Server"
+while true; do
+  if tail output.log | grep -q "joined the game" ; then
+    break
+  fi
+  sleep 1
+done
+echo "Done"
---
+++ start_spigot.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cd Minecraft_Tools
+cd server
+
+java -Xms512M -Xmx1024M -jar -DIReallyKnowWhatIAmDoingISwear spigot-1.19.4.jar nogui -o true > output.log &
+
+echo "Starting Spigot Server"
+while true; do
+    if tail output.log | grep -q "Done" ; then
+        break
+    fi
+    sleep 1
+done
+echo "Done"
